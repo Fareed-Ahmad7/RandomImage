75b492787ed15a4056077fbfe9e0a5f5b0317ec9
Fareed-Ahmad7 <fareedahmad1097@gmail.com>
2023-08-28 23:02:09 +0530

    initial commit

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let imageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button : UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Generate Image", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(OnButtonTap), for: .touchUpInside)
+        randomImage()
     }
-
+    
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemGreen,
+        .systemYellow,
+        .systemPurple,
+        .systemGray,
+        .systemCyan
+    ]
+    
+    @objc func OnButtonTap(){
+        randomImage()
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom , width: view.frame.size.width-60, height: 55)
+    }
+    
+    func randomImage(){
+        let link = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: link)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
 
 }
 
